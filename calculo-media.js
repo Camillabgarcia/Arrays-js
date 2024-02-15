7d7e75deeacd4625c9f4f39ec62dfd923418142b
Camillabgarcia <camillabgarciaa@hotmail.com>
2024-02-15 19:42:52 -0300

    calculo-media

--- calculo-media.js
+++ calculo-media.js
@@ -3,7 +3,7 @@
 //const nota3 = 8;
 //const nota4 = 7.5;
 
-const notas = [10, 6.5, 8, 7, 5];    //0, 1, 2 e 3.
+const notas = [10, 6.5, 8, 7, 5];    //[0], [1], [2] e [3] (índice do array).  //Valores dos índices : [10, 6.5, 8, 7, 5].
 
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;  //LENGTH (COMPRIMENTO): tamanho da lista de forma automática.
 
