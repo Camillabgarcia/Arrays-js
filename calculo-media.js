b2bba6614015c6bbe0734e5105e753dc94ded4d3
Camillabgarcia <camillabgarciaa@hotmail.com>
2024-02-15 19:34:50 -0300

    calculo-media

--- calculo-media.js
+++ calculo-media.js
@@ -1,8 +1,10 @@
-const nota1 = 10;
-const nota2 = 6.5;
-const nota3 = 8;
-const nota4 = 7.5;
+//const nota1 = 10;
+//const nota2 = 6.5;
+//const nota3 = 8;
+//const nota4 = 7.5;
 
-const media = (nota1 + nota2 + nota3 + nota4) / 4;
+const notas = [10, 6.5, 8, 7, 5];    //0, 1, 2 e 3.
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;  //LENGTH (COMPRIMENTO): tamanho da lista de forma automática.
 
 console.log(media);
